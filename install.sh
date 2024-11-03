3f4d11bf3f828bd299cfc03b6ea359a68a8fa188
hansen1416 <badapplesweetie@gmail.com>
2024-11-03 22:16:54 +0800

    1

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # conda create -n tram python=3.10 -y
 # conda activate tram
 
-conda install nvidia/label/cuda-11.8.0::cuda-toolkit -y # you can disable this if you already have cuda-11.8
-conda install pytorch==2.0.0 torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
+# conda install nvidia/label/cuda-11.8.0::cuda-toolkit -y # you can disable this if you already have cuda-11.8
+conda install pytorch torchvision torchaudio pytorch-cuda -c pytorch -c nvidia -y
 pip install 'git+https://github.com/facebookresearch/detectron2.git@a59f05630a8f205756064244bf5beb8661f96180'
 pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"
 
@@ -27,6 +27,6 @@ pip install timm==0.6.7
 pip install evo
 pip install pytorch-minimize
 pip install imageio[ffmpeg]
-pip install numpy==1.23
+pip install numpy==1.26
 pip install gdown
 
