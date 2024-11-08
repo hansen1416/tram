e65b3f4cc2c5ddbbfa56b0f7e8ab8ef0f56f2924
hansen1416 <badapplesweetie@gmail.com>
2024-11-08 16:21:59 +0800

    1

--- install_conda.sh
+++ install_conda.sh
@@ -4,8 +4,6 @@ curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 
 bash Anaconda3-2024.10-1-Linux-x86_64.sh -b -p $HOME/anaconda3
 
-git clone --recursive https://github.com/hansen1416/tram
-
 source ~/.bashrc
 
 conda create -n tram python=3.10 -y
