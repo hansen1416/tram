049fc493533ab5ece16cbe9fa3704d68babfdec9
hansen1416 <badapplesweetie@gmail.com>
2024-11-17 21:43:52 +0800

    1

--- batch_process.py
+++ batch_process.py
@@ -2,11 +2,23 @@ import os
 import subprocess
 
 target_folder = os.path.join("/tram", "videos")
+results_folder = os.path.join("/tram", "results")
 
 # list all files in `target_folder`
 files = os.listdir(target_folder)
 
+print(f"got {len(files)} files")
+
 for f in files:
+
+    file_name = f.split(".")[0]
+
+    # check if the results already exists
+    if os.path.exists(os.path.join(results_folder, file_name)):
+        print(f"{f} already processed")
+        continue
+    else:
+        print(f"{f} in process")
 
     video_path = os.path.join(target_folder, f)
 
@@ -20,8 +32,8 @@ for f in files:
             "--static_camera",
         ],
         ["python", "scripts/estimate_humans.py", "--video", video_path],
-        ["python", "scripts/visualize_tram.py", "--video", video_path],
+        # ["python", "scripts/visualize_tram.py", "--video", video_path],
     ]
 
     for command in commands:
-        subprocess.run(command, capture_output=True, text=True, check=True)
+        subprocess.run(command, check=True)
