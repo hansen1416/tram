a553c8057c40678b0fd460359da1ba4ac892b88e
hansen1416 <badapplesweetie@gmail.com>
2024-11-21 11:25:33 +0800

    1

--- install.sh
+++ install.sh
@@ -33,7 +33,6 @@ pip install pytorch-minimize
 pip install imageio[ffmpeg]
 pip install numpy==1.26
 pip install gdown
-# pip install oss2
 
 # "https://download.pytorch.org/models/resnet50-19c8e357.pth" to /home/hlz/.cache/torch/hub/checkpoints/resnet50-19c8e357.pth
 # "https://download.pytorch.org/models/resnet18-5c106cde.pth" to /home/hlz/.cache/torch/hub/checkpoints/resnet18-5c106cde.pth
